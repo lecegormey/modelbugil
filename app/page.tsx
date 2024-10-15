31d7fb51005585f159159c0bad4ecdd943f34e8c
lecegormey <lecegormey@gmail.com>
2024-10-15 20:32:27 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,13 +17,12 @@ export default async function Home({
     const query = (searchParams.q && (searchParams.q as string)) || undefined;
 
     return (
-        <div className="md:my-2">
+        <div className="md:my-2"><h1>{SITENAME}</h1><h2>Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru</h2>
             {query ? (
                 <SearchCardList query={query} banner />
             ) : (
                 <CardList page={page} per_page={per_page} fld_id={fld_id} />
             )}
-		<h1>{SITENAME}</h1><h2>Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru</h2>
         </div>
     );
 }
